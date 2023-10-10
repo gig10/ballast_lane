136f8e0be06a38a0ecd473631a3fc912f1568ff4
Giovanni <giovanni.silva@outlook.com>
2023-10-10 12:29:27 -0300

    added new controller for games endpoint

---
+++ src/GameDatabase.API/GamesEndpoints/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameDatabase.API.GameEndpoints
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+    }
+}
